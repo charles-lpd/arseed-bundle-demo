3526aeee5734878e2c5d66d35723467756a90eb6
lpd0916 <1985413702@qq.com>
2024-01-19 17:46:25 +0800

    chore: update

--- src/components/ConnectWallet.vue
+++ src/components/ConnectWallet.vue
@@ -4,13 +4,18 @@
     <button class="btn btn-danger" @click="connectMetamask" v-show="!connected">connectMetamask</button><br/>
     <br/>
     <button class="btn btn-danger" @click="connectArconnect" v-show="!connected">connectArconnect</button>
+    <br/>
+    <br/>
+    <button class="btn btn-danger" @click="loginEverId" v-show="!connected">loginEverId</button>
   </div>
 </template>
 
 
 <script>
-import { genAPI,genArweaveAPI } from "arseeding-js";
+// import { genArweaveAPI, genAPI } from "arseeding-js";
 import pubsub from 'pubsub-js'
+import Everpay from 'everpay'
+import { getWebAuthAPI, genAPI, genArweaveAPI } from 'arseeding-js-fido'
 export default {
   name: "ConnectWallet",
   data() {
@@ -34,6 +39,32 @@ export default {
       this.connected = true
       instance.addr = this.signerAddr
       pubsub.publish('connected', instance)
+    },
+    async loginEverId(){
+      try{
+        const everpay = new Everpay({
+          debug:true
+        })
+        const params = {
+          everpay: everpay,
+          publicKey: 'eyJJRCI6IjBIMGZjWWwwRHJyZjQveGo0L0RMYlpFajhud1JsZUMxcmFVSTN0Qm8rblU9IiwiUHVibGljS2V5IjoicFFFQ0F5WWdBU0ZZSUZYK1VaWENqRlZ2ajVucE0xME56ZzdjYmQ2bkVISjFLOVg4M3RnejFmM3VJbGdnUWFrWHJWeXptMkc0WndzUEZUSjR2dEVDcFpqTEpDelAzNlZBbWpFYVFtST0iLCJBdHRlc3RhdGlvblR5cGUiOiJwYWNrZWQiLCJUcmFuc3BvcnQiOlsiaW50ZXJuYWwiXSwiRmxhZ3MiOnsiVXNlclByZXNlbnQiOnRydWUsIlVzZXJWZXJpZmllZCI6dHJ1ZSwiQmFja3VwRWxpZ2libGUiOmZhbHNlLCJCYWNrdXBTdGF0ZSI6ZmFsc2V9LCJBdXRoZW50aWNhdG9yIjp7IkFBR1VJRCI6InJjNEFBalc4eGdwa2l3c2w4ZkJWQXc9PSIsIlNpZ25Db3VudCI6MCwiQ2xvbmVXYXJuaW5nIjpmYWxzZSwiQXR0YWNobWVudCI6InBsYXRmb3JtIn19',
+          debug:true
+        }
+        const instance = await getWebAuthAPI(params)
+        console.log(instance, 'instance')
+        this.signerAddr = instance.signer.signer.account
+        this.connected = true
+        pubsub.publish('connected', {...instance,addr:this.signerAddr})
+        // const everpayAsync = {
+        //   addr:this.signerAddr,
+        //   ...everpaytest
+        // }
+
+        // console.log(instance)
+
+      }catch(e){
+        console.log(e)
+      }
     }
   }
 }
--- src/components/Upload.vue
+++ src/components/Upload.vue
@@ -2,10 +2,10 @@
   <div class="upload-img-root">
     <el-select v-show="connected" v-model="selectedSymbol" class="m-2" placeholder="Select" size="large">
       <el-option
-        v-for="symbol in symbols"
-        :key="symbol"
-        :label="`${symbol} ${balanceStack[symbol]}`"
-        :value="symbol"
+        v-for="(item,index) in symbols"
+        :key="index"
+        :label="`${item.symbol} ${balanceStack[item.symbol]}`"
+        :value="item.symbol"
       />
     </el-select>
     <span v-if="connected">{{balance}}</span>
@@ -32,19 +32,24 @@
         </li>
       </ul>
     </div>
+    <button v-if="connected" @click="testSignMessageAsync">
+    testSignMessageAsync
+    </button>
   </div>
 </template>
 
 <script>
 import pubsub from 'pubsub-js'
 import Everpay from 'everpay'
-import { getBundleFee, getOrders } from 'arseeding-js'
+import { getBundleFee, getOrders, getTokenTagByEver } from 'arseeding-js'
 import Bignumber from 'bignumber.js'
+import { genEverId, isSmartAccount } from 'everpay/esm/utils/util'
+import { getWebAuthAPI } from 'arseeding-js-fido'
 function  getArseedUrl() {
   let arseedUrl = "https://arseed.web3infra.dev"
   const hostname = window.location.hostname
   if ( hostname.split(".")[0].indexOf("dev") !== -1 || hostname === "localhost") { // test env
-    arseedUrl = "https://arseed-dev.web3infra.dev"
+    arseedUrl = "https://arseed.web3infra.dev"
   }
   return arseedUrl
 }
@@ -67,12 +72,16 @@ export default {
     };
   },
   watch: {
-    selectedSymbol() {
-      if (this.everpay.balance && this.selectedSymbol && this.instance.addr) {
+    async selectedSymbol() {
+      console.log(this.balance, '1', this.selectedSymbol, '2',  this.instance.addr)
+      if (this.selectedSymbol && this.instance.addr) {
+       const token = await getTokenTagByEver(this.selectedSymbol)
+       console.log(token[0])
         this.everpay.balance({
-          symbol: this.selectedSymbol,
+          tag: token[0],
           account: this.instance.addr
         }).then(result => {
+          console.log(result)
           this.balance = result
         })
       }
@@ -88,28 +97,31 @@ export default {
     async combineFileList(files) {
       const file = files[files.length - 1]
       console.log('file.size', file.size)
-
-      const fee = await getBundleFee(this.arseedUrl, file.size, this.selectedSymbol)
+      const token = this.symbols.find((t)=> t.symbol === this.selectedSymbol)
+      const fee = await getBundleFee(this.arseedUrl, file.size, token.symbol)
       const formatedFee = new Bignumber(fee.finalFee).dividedBy(new Bignumber(10).pow(fee.decimals)).toString()
+      console.log(formatedFee, 'formatedFee')
+      console.log(this.balance, 'this.balance')
       if (+this.balance >= +formatedFee) {
         const reader = new FileReader();
         reader.readAsArrayBuffer(file.raw);
         reader.onload= async ()=> {
           const data = reader.result
           const ops = {
-            tags: [{name: "FileName", value:file.name},{name: "Content-Type",value:file.raw.type}]
+            tags: [{name: "FileName", value:file.name},{name: "Content-Type",value:file.raw.type},{name:"eid", value: genEverId(this.instance.addr)}]
           }
-          const res = await this.instance.sendAndPay(this.arseedUrl, data, this.selectedSymbol, ops)
-          console.log(res)
+          const res = await this.instance.sendAndPay(this.arseedUrl, data, token.tag, ops)
+          console.log(res, 'res demo')
           this.submitResp = JSON.stringify(res)
           this.getOrders()
         }
       } else {
-        alert(`need ${formatedFee} ${this.selectedSymbol} to upload`)
+        alert(`need ${formatedFee} ${token.symbol} to upload`)
       }
     },
     async getOrders() {
-      getOrders(this.arseedUrl, this.instance.addr).then(orders => {
+      const acc  = isSmartAccount(this.instance.addr) ? genEverId(this.instance.addr) : this.instance.addr
+      getOrders(this.arseedUrl, acc).then(orders => {
         this.orders = orders
       })
     },
@@ -129,14 +141,40 @@ export default {
         })
         this.balanceStack = balanceStack
       })
+    },
+    async testSignMessageAsync() {
+      const params = {
+        everpay: this.instance,
+        publicKey: 'eyJJRCI6InVCVithc3h0a3JrU0xmb3pqVkNuL1E9PSIsIlB1YmxpY0tleSI6InBRRUNBeVlnQVNGWUlJeWQ0ckhxbjlQUHFaZW5wZ1Erbk95Nmp3VFozK08zUVhIbVZ2ZzJjQnpPSWxnZ3E2d21XVmxNSGFPUFNGSjBHU2ovdU00TEtUVjg1SDc4NUZPOThkM09oS0E9IiwiQXR0ZXN0YXRpb25UeXBlIjoibm9uZSIsIlRyYW5zcG9ydCI6WyJpbnRlcm5hbCJdLCJGbGFncyI6eyJVc2VyUHJlc2VudCI6dHJ1ZSwiVXNlclZlcmlmaWVkIjp0cnVlLCJCYWNrdXBFbGlnaWJsZSI6dHJ1ZSwiQmFja3VwU3RhdGUiOnRydWV9LCJBdXRoZW50aWNhdG9yIjp7IkFBR1VJRCI6InV0cFZacWVxUUIrOWxrVmhtbFVTRFE9PSIsIlNpZ25Db3VudCI6MCwiQ2xvbmVXYXJuaW5nIjpmYWxzZSwiQXR0YWNobWVudCI6InBsYXRmb3JtIn19',
+        account: this.instance.addr
+      }
+      const data = await getWebAuthAPI(params)
+      console.log(data, 'data')
+      const options = {
+        tags: [
+          { name: 'Content-Type', value: 'text/plain' },
+          { name: 'aa', value: 'aaa' }
+        ]
+      }
+      const res = await data.sendAndPay(this.arseedUrl, 'submit data', 'arweave,ethereum-ar-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA,0x4fadc7a98f2dc96510e42dd1a74141eeae0c1543', options)
+      console.log(res, 'res')
+      // const state = await this.instance.signMessageAsync({isSmartAccount:true,debug:true, account: this.instance.addr},'submit data')
+      // console.log(state, 'testSignMessageAsync data')
     }
   },
-  mounted() {
-    this.everpay = new Everpay()
-    this.everpay.info().then(info => {
-      this.symbols = info.tokenList.map(token => token.symbol)
-      this.selectedSymbol = this.symbols[0]
+  async mounted() {
+    this.everpay = new Everpay({debug:true})
+    console.log(this.everpay)
+   const info = await  this.everpay.info()
+    console.log(info, 'info')
+    this.symbols = info.tokenList.map(token => {
+      return {
+        symbol: token.symbol,
+        tag: token.tag
+      }
     })
+    this.selectedSymbol = this.symbols[0].symbol
+    console.log(this.selectedSymbol, 'info')
 
     this.pubId = pubsub.subscribe('connected',async (msgName,data)=>{
       this.connected = true
